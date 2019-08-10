a3bd5da0d60c5d3c277518fc9e7a63a733f2a048
Kevin Honnoll <khonnoll@gmail.com>
2019-08-09 21:36:41 -0400

    modified user migration file to include name and replace password with password_digest

--- db/migrate/20190809185911_create_users.rb
+++ db/migrate/20190809185911_create_users.rb
@@ -2,8 +2,9 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :username
-      t.string :password
+      t.string :password_digest
       t.boolean :admin
+      t.string :name
       t.string :instrument
       t.string :location
       t.integer :balance_due
