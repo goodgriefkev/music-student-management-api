ec427db5b3fa90fb8e01a9760aaf5c44d9c0cf68
Kevin Honnoll <khonnoll@gmail.com>
2019-08-10 13:00:15 -0400

    modified routes to only create an assignemnt with an associated user, and to prevent show route access on assignemnts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :assignments
+  resources :assignments, except: [:show]
   resources :users do
     collection do
       post '/login', to: 'users#login'
     end
-    resources :assignments
+    resources :assignments, only: [:create]
     end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
