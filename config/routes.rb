89fc7c9e26777f59f23b802f043f9987935082a2
Kevin Honnoll <khonnoll@gmail.com>
2019-08-10 10:30:14 -0400

    nested assignments in users in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     collection do
       post '/login', to: 'users#login'
     end
+    resources :assignments
     end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
